87d83b4e432e4228a8b51a24090ce59710cd5a77
EssenceCode <123456789+odin@users.noreply.github.com>
2023-07-06 11:00:06 +0800

    created a function to dipslay books

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.card-container');
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -9,8 +9,41 @@ function Book(title, author, pages, read) {
     this.read = read
 };
   
-function addBooktoLibrary(title, author, pages, read) {
+function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
-    myLibrary.push(book)
-    
+    myLibrary.push(book);
+    container.textContent = '';
+    displayBook();
+
 };
+
+function displayBook() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        myLibrary[i];
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const bookTitle = document.createElement('p');
+        bookTitle.classList.add('bookTitle')
+        const bookAuthor = document.createElement('p');
+        bookAuthor.classList.add('bookAuthor')
+        const bookPages = document.createElement('p');
+        bookPages.classList.add('bookPages')
+        const bookRead = document.createElement('p');
+        bookRead.classList.add('bookRead')
+
+        bookTitle.textContent = `Title: ${myLibrary[i].title}`;
+        bookAuthor.textContent = `Author: ${myLibrary[i].author}`;
+        bookPages.textContent = `Pages: ${myLibrary[i].pages}`;
+        bookRead.textContent = `Read: ${myLibrary[i].read}`;
+
+        card.appendChild(bookTitle);
+        card.appendChild(bookAuthor);
+        card.appendChild(bookPages);
+        card.appendChild(bookRead);
+
+        container.appendChild(card);
+
+    
+    }
+
+}
