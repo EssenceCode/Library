223983c6170d1267e3cc44b86b73302d4c3a6e51
EssenceCode <123456789+odin@users.noreply.github.com>
2023-07-07 11:11:52 +0800

    added a button to change reading status of book

--- script.js
+++ script.js
@@ -24,6 +24,13 @@ function Book(title, author, pages, read) {
     this.author = author
     this.pages = pages
     this.read = read
+    this.readStatus = function() {
+        if(this.read === 'yes') {
+            return this.read = 'no';
+        } else if(this.read === 'no') {
+            return this.read = 'yes';
+        }
+    }
 };
   
 function addBookToLibrary(title, author, pages, read) {
@@ -52,25 +59,36 @@ function displayBook() {
         const bookRead = document.createElement('p');
         bookRead.classList.add('bookRead');
         const removeBook = document.createElement('button');
+        const readStatus = document.createElement('button');
+
        
         bookTitle.textContent = `Title: ${myLibrary[i].title}`;
         bookAuthor.textContent = `Author: ${myLibrary[i].author}`;
         bookPages.textContent = `Pages: ${myLibrary[i].pages}`;
         bookRead.textContent = `Read: ${myLibrary[i].read}`;
         removeBook.textContent = 'Remove';
+        readStatus.textContent = 'Read';
+
         removeBook.addEventListener('click', (e) => {
             card.textContent = '';
+        });
+        readStatus.addEventListener('click', (e) => {
+            bookRead.textContent = `Read: ${myLibrary[i].readStatus()}`
+            console.log(myLibrary[i].read)
         })
+
         card.appendChild(bookTitle);
         card.appendChild(bookAuthor);
         card.appendChild(bookPages);
         card.appendChild(bookRead);
         card.appendChild(removeBook);
+        card.appendChild(readStatus);
+
 
 
         container.appendChild(card);
 
-    
+   
     }
 
 };
